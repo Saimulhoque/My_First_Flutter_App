b5122c2354c4480dd829b472b2112f6bcc6d86a0
Saimulhoque <saimulhoque8217@gmail.com>
2022-01-03 23:22:42 +0600

    11:22 Pm

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+double conHeight = 80;
+double conWidth = 80;
+
+String imgsrc = "https://st.depositphotos.com/1006706/2671/i/600/depositphotos_26715369-stock-photo-which-way-to-choose-3d.jpg";
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -24,17 +29,53 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    // double height = MediaQuery.of(context).size.height;
+    // double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: Text("My first flutter app", style: TextStyle(color: Colors.white),),
+        title: Text("My first app", style: TextStyle(color: Colors.white),),
       ),
-
       body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
         color: Colors.green,
-        child: Center(child: Text("Welcome To My First Flutter App")),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              InkWell(
+                onTap: (){
+                  setState(() {
+                    conHeight==300;
+                    conWidth=300;
+                  });
+                },
+
+                onDoubleTap: (){
+                  setState(() {
+                    conHeight=100;
+                    conWidth=100;
+                  });
+                },
+                child: AnimatedContainer(
+                  height: conHeight,
+                  width: conWidth,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    image: DecorationImage(
+                        image: NetworkImage("https://st.depositphotos.com/1006706/2671/i/600/depositphotos_26715369-stock-photo-which-way-to-choose-3d.jpg"),
+                      fit: BoxFit.cover
+                    ),
+                    borderRadius: BorderRadius.circular(180),
+                    border: Border.all(color: Colors.yellow, width: 4)
+                  ), duration: Duration(milliseconds: 4000),
+                ),
+              ),
+              Image.asset("assets/image/image1.jpg"),
+              Image.asset("assets/image/image2.jpg"),
+              Image.asset("assets/image/image1.jpg"),
+              Image.asset("assets/image/image2.jpg"),
+            ],
+          ),
+        ),
       ),
     );
   }
